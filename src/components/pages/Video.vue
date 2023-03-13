6151beb1459e18b07a7cd6d67ef69373a0bff5b7
Alexart21 <iskander.m.211@gmail.com>
2023-03-13 13:11:44 +0300

    next

---
+++ src/components/pages/Video.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>HTML5 video</h1>
+  <video width="400" height="300" controls="controls">
+     <source src="./../../assets/video/_video.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
+     <!-- <source src="http://alexart.laravel/test/video" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'> -->
+</video>
+</template>
+
+
